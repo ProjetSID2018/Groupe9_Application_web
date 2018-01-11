c06c0986a975bef9976e95eda77b72edc1bdab70
Unknown <yayabinani@gmail.com>
2018-01-11 16:33:38 +0100

    page de garde

--- code/WEB/dynamic/header.js
+++ code/WEB/dynamic/header.js
@@ -3,23 +3,27 @@
 /*utilise le lien vers bootstrap de chaque page */
 
 document.body.insertAdjacentHTML("afterBegin",
-  '<header id="header">'+
-  '<div class="container-fluid" id="div_header">'+
-    '<div class="page-header" id="sub_div_header">'+
-      '<nav class="navbar navbar-default navbar-fixed-top" id="bar_header">'+ /* fixe le menu en haut de page body {padding-top: 70px;} pour le décoler de 70 px */
-        '<div class="container-fluid" id="div_logo_header">'+
-          '<div class="navbar-header" id="logo_bar_header">'+
-            '<a class="navbar-brand" href="#" id="logo_header">WATCHNEWS</a>'+
-          '</div>'+
-          '<ul class="nav navbar-nav navbar-right" id="bar_page_choise_header">'+
-            '<li class="active" id="bouton_accueil_header"><a href="#" id="accueil_link_header">Accueil</a></li>'+
-            '<li id="bouton_themes_header"><a href="#" id="themes_link_header">Thèmes</a></li>'+
-            '<li id="bouton_recherche_header"><a href="#" id="recherche_link_header">Recherche</a></li>'+
-          '</ul>'+
-        '</div>'+
-      '</nav>'+
-    '</div>'+
-  '</div>'+ 
-  '</header>');
+    '<header>'+
+'<nav class="navbar navbar-default">'+      
+     '<div class="container">'+
+       '<div class="navbar-header">'+
+         '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#codebrainery-toggle-nav" aria-expanded="false">'+
+           '<span class="sr-only">Toggle navigation</span>'+
+           '<span class="icon-bar"></span>'+
+           '<span class="icon-bar"></span>'+
+           '<span class="icon-bar"></span>'+
+         '</button>'+
+         '<a class="navbar-brand" href="#">Watchnews</a>'+
+       '</div>'+
 
+       '<div class="collapse navbar-collapse" id="codebrainery-toggle-nav">'+
+         '<ul class="nav navbar-nav navbar-right">'+
+           '<li><a href="../../../index.html">Accueil</a></li>'+
+           '<li><a href="code/WEB/html/theme.html">Thèmes</a></li>'+
+           '<li><a href="code/WEB/html/research_page.html">Recherche</a></li>'+
+         '</ul>'+
+       '</div>'+
 
+     '</div>'+
+'</nav>'+
+'</header>');
