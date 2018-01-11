973adb070c9a311ada10c12e24bbad4037a428a7
Unknown <yayabinani@gmail.com>
2018-01-11 09:11:57 +0100

    MAJ J3

--- code/WEB/dynamic/footer.js
+++ code/WEB/dynamic/footer.js
@@ -1,50 +1,27 @@
 
 /*ajout du lien necessaire pour les logos (font-incone) du footer*/
 
-
-
 document.head.insertAdjacentHTML("beforeend",'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
-
-
 
 /*fonction servant a inserer un footer dans les pages web. Permet d'actualiser tous les footer en meme temps.*/
 /*utilise le lien vers bootstrap de chaque page */
 
-
-
 document.body.insertAdjacentHTML("beforeend",
-	'<footer id ="ft1">'+
-    '<div class="top-bar">'+
-
-      '<div class="container-fluid">'+
-
-       '<div class="row">'+
-
-          '<div class = "text-left">'+
-          
-              '<a href="#" class="navbar-left"><img src="https://archive.is/o4KHi/3e85f3ee0e66b11b1b91af32d2568fb284561469.png" ></a>'+
-
-            '</div>'+
-
-            '<div class="socl-icons text-right" >'+
-
-              '<ul  class="list-inline ">'+
-
-                  '<li><a href="#"><i class="fa fa-facebook fa-5x" aria-hidden="true"></i></a> </li>'+
-
-                  '<li><a href="#"><i class="fa fa-twitter fa-5x" aria-hidden="true"></i></a></li>'+
-
-                  '<li><a href="#"><i class="fa fa-linkedin fa-5x" aria-hidden="true"></i></a></li>'+
-
-                  '<li><a href="#"><i class="fa fa-google-plus fa-5x" aria-hidden="true"></i></a></li>'+
-
-            '</ul>'+
-
+	'<footer id ="footer">'+
+    '<div class="top-bar" id="bar_footer">'+
+      '<div class="container-fluid" id="div_footer">'+
+        '<div class="row" id="div_row_footer">'+
+          '<div class = "text-left" id="sid_div_footer">'+
+            '<a href="#" class="navbar-left" id="sid_link_footer"><img src="https://archive.is/o4KHi/3e85f3ee0e66b11b1b91af32d2568fb284561469.png" id="sid_logo_footer"></a>'+
           '</div>'+
-
+          '<div class="socl-icons text-right" id="social_icons_div_footer">'+
+            '<ul  class="list-inline" id="social_icons_set_cover">'+
+              '<li id="facebook_link_footer"><a href="#"><i class="fa fa-facebook fa-5x" aria-hidden="true" id="facebook_icons_footer"></i></a> </li>'+
+              '<li id="twitter_link_footer"><a href="#"><i class="fa fa-twitter fa-5x" aria-hidden="true" id="twitter_icons_footer"></i></a></li>'+
+              '<li id="google_plus_link_footer"><a href="#"><i class="fa fa-google-plus fa-5x" aria-hidden="true" id="google_plus_icons_footer"></i></a></li>'+
+            '</ul>'+
+          '</div>'+
         '</div>'+
-
       '</div>'+
-
     '</div>'+
-    '</footer>');
+  '</footer>');
--- code/WEB/dynamic/header.js
+++ code/WEB/dynamic/header.js
@@ -1,42 +1,25 @@
 
-
-
-/*fonction servant a inserer un header dans les pages web. Permet d'actualiser tous les headers en meme temps.*/
+/*fonction servant a inserer le header dans l'ensemble des pages du site. Permet d'actualiser tous les headers en meme temps.*/
 /*utilise le lien vers bootstrap de chaque page */
 
-
-
 document.body.insertAdjacentHTML("afterBegin",
-  '<header id="hd1">'+
-  '<div class="container-fluid">'+
-
-    '<div class="page-header">'+
-
-      '<nav class="navbar navbar-default navbar-fixed-top">'+ /* fixe le menu en haut de page body {padding-top: 70px;} pour le décoler de 70 px */
-
-        '<div class="container-fluid">'+
-
-          '<div class="navbar-header">'+
-
-            '<a class="navbar-brand" href="#">WATCHNEWS</a>'+
-
+  '<header id="header">'+
+  '<div class="container-fluid" id="div_header">'+
+    '<div class="page-header" id="sub_div_header">'+
+      '<nav class="navbar navbar-default navbar-fixed-top" id="bar_header">'+ /* fixe le menu en haut de page body {padding-top: 70px;} pour le décoler de 70 px */
+        '<div class="container-fluid" id="div_logo_header">'+
+          '<div class="navbar-header" id="logo_bar_header">'+
+            '<a class="navbar-brand" href="#" id="logo_header">WATCHNEWS</a>'+
           '</div>'+
-
-            '<ul class="nav navbar-nav navbar-right">'+
-
-              '<li class="active"><a href="#">Accueil</a></li>'+
-
-              '<li><a href="#">Thèmes</a></li>'+
-
-              '<li><a href="#">Recherche</a></li>'+
-
-            '</ul>'+
-
+          '<ul class="nav navbar-nav navbar-right" id="bar_page_choise_header">'+
+            '<li class="active" id="bouton_accueil_header"><a href="#" id="accueil_link_header">Accueil</a></li>'+
+            '<li id="bouton_themes_header"><a href="#" id="themes_link_header">Thèmes</a></li>'+
+            '<li id="bouton_recherche_header"><a href="#" id="recherche_link_header">Recherche</a></li>'+
+          '</ul>'+
         '</div>'+
-
       '</nav>'+
-
     '</div>'+
-    '</header>');
+  '</div>'+ 
+  '</header>');
 
 
