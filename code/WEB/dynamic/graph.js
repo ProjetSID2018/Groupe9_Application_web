aa964b797498e65e3e29cc7bc407c17c6f488c18
Unknown <yayabinani@gmail.com>
2018-01-12 09:34:26 +0100

    MAJ SERVER

---
+++ code/WEB/dynamic/graph.js
@@ -0,0 +1,68 @@
+function start() {
+	request_top_10();
+    request_word_cloud();
+}
+
+function request_top_10() {
+	$('#top10_sources').hide();
+    $.ajax({
+        url:'http://localhost:5000/test',
+        type: 'GET',
+        dataType: 'json',
+        success: drawBasic,
+        error: ajax_failed,
+    });
+}
+
+function request_word_cloud() {
+    $('#word_cloud_cover').hide();
+    $.ajax({
+        url:'http://localhost:5000/test1',
+        type: 'GET',
+        dataType: 'json',
+        success: draw_cloud,
+        error: ajax_failed,
+    });
+}
+
+
+function drawBasic(data_json) {
+	google.charts.load('visualization', '1', {packages: ['corechart', 'bar']});
+	google.charts.setOnLoadCallback(function(){
+	var data = new google.visualization.DataTable();
+	data.addColumn("string", "sources");
+    data.addColumn("number", "nombres d'articles");
+    for (var i = 1; i <11; i++) {
+    	data.addRow([data_json[i].source,data_json[i].nombre]);
+	}
+	var options = {
+    	hAxis: {
+        	title: 'sources',
+        	format: 'string',
+		},
+        vAxis: {
+        	title: "nombres d'articles par source"
+		}
+	};
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	$('#top10_sources').show();
+	chart.draw(data, options);
+});
+}
+
+function draw_cloud(some_words) {
+	var tab = [];
+	for (var i = 1; i <11; i++) {
+		tab = tab.concat(some_words[i]);
+	}
+	$(document).ready(function() {
+	  $(".word_cloud_row_graph").jQCloud(tab);
+    });
+
+	 $('#word_cloud_cover').show();
+}
+
+
+function ajax_failed() {
+    alert('erreur');
+}
---
+++ server.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 12:22:10 2018
+
+@author: G9
+"""
+
+from flask import Flask, request,  jsonify, Response
+import json
+from flask_restful import Resource, Api
+import json
+app = Flask(__name__)
+api = Api(app)
+
+
+# @app.route('/general', methods = ['POST'])
+# def general():
+#     #--> requete sur la base
+#     #--> appel de l'API du groupe statiques
+#     
+#     data = {
+#         'nbart'  : 2000,
+#         'source' : ["lemonde", "le figaro", "le nouvel obs"]
+#     }
+#     resp = jsonify(data)
+#     resp.status_code = 200
+#     resp.headers.add('Access-Control-Allow-Origin', '*')
+#     return resp
+# 
+# @app.route('/theme', methods = ['GET'])
+# def theme():
+#     #--> requete sur la base
+#     #--> appel de l'API du groupe statiques
+#     word =request.args.get('word')  
+#     source = request.args.get('source')  
+#     print(word, source)
+#     data = {
+#         'src'  : source,
+#         'word' :word,
+#         'tendancy' : {'month':'-','week':'+','day':'+-'},
+#         'verbe':{'verbe' : 'atomiser','month':'+','week':'-','day':'--'},
+#         'theme':{'culture' : 1,'politic':80,'humor':19}
+#     }
+#     resp = jsonify(data)
+#     resp.status_code = 200
+#     resp.headers.add('Access-Control-Allow-Origin', '*')
+#     return resp
+    
+@app.route('/test', methods = ['GET'])
+def general():
+    #--> requete sur la base
+    #--> appel de l'API du groupe statiques
+    
+    data = {
+   '1': {
+        "source": "figaro",
+        "nombre" : 210
+    },
+    '2':{
+        "source": "monde",
+        "nombre": 2015
+    },
+    '3':{
+        "source": "depeche",
+        "nombre" : 50
+    },
+    '4':{
+        "source": "set",
+        "nombre": 45
+    },
+    '5':{
+        "source": "truc",
+        "nombre" : 544
+    },
+    '6':{
+        "source": "ouai",
+        "nombre": 45
+    },
+    '7':{
+        "source": "plus",
+        "nombre" : 76
+    },
+    '8':{
+        "source": "trente",
+        "nombre": 71
+    },
+    '9':{
+        "source": "aller",
+        "nombre" : 828
+    },
+    '10':{
+        "source": "test",
+        "nombre": 783
+    }}
+    resp = jsonify(data)
+    resp.status_code = 200
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp  
+   
+@app.route('/test1', methods = ['GET'])
+def general1():
+    #--> requete sur la base
+    #--> appel de l'API du groupe statiques
+    
+    data = {
+   '1': {
+        "text": "figaro",
+        "weight" : 1
+    },
+    '2':{
+        "text": "monde",
+        "weight": 2
+    },
+    '3':{
+        "text": "depeche",
+        "weight" : 3
+    },
+    '4':{
+        "text": "set",
+        "weight": 4
+    },
+    '5':{
+        "text": "truc",
+        "weight" : 5
+    },
+    '6':{
+        "text": "ouai",
+        "weight": 6
+    },
+    '7':{
+        "text": "plus",
+        "weight" : 7
+    },
+    '8':{
+        "text": "trente",
+        "weight": 8
+    },
+    '9':{
+        "text": "aller",
+        "weight" : 9
+    },
+    '10':{
+        "text": "test",
+        "weight": 10
+    }}
+    resp = jsonify(data)
+    resp.status_code = 200
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    return resp
+
+   
+if __name__ == '__main__':
+  app.run()
