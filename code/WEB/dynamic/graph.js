0cd4d47debdbdfb0f32b68286492fd678d2eefe9
FlorenceGourmelon <florence.gourmelon@outlook.fr>
2018-01-12 09:44:02 +0000

    Update graph.js

--- code/WEB/dynamic/graph.js
+++ code/WEB/dynamic/graph.js
@@ -29,25 +29,25 @@ function request_word_cloud() {
 function drawBasic(data_json) {
 	google.charts.load('visualization', '1', {packages: ['corechart', 'bar']});
 	google.charts.setOnLoadCallback(function(){
-	var data = new google.visualization.DataTable();
-	data.addColumn("string", "sources");
-    data.addColumn("number", "nombres d'articles");
-    for (var i = 1; i <11; i++) {
-    	data.addRow([data_json[i].source,data_json[i].nombre]);
-	}
-	var options = {
-    	hAxis: {
-        	title: 'sources',
-        	format: 'string',
-		},
-        vAxis: {
-        	title: "nombres d'articles par source"
+		var data = new google.visualization.DataTable();
+		data.addColumn("string", "sources");
+    		data.addColumn("number", "nombres d'articles");
+    		for (var i = 1; i <11; i++) {
+    			data.addRow([data_json[i].source,data_json[i].nombre]);
 		}
-	};
-	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-	$('#top10_sources').show();
-	chart.draw(data, options);
-});
+		var options = {
+    		hAxis: {
+        		title: 'sources',
+        		format: 'string',
+		},
+        	vAxis: {
+        		title: "nombres d'articles par source"
+		}
+		};
+		var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+		$('#top10_sources').show();
+		chart.draw(data, options);
+	});
 }
 
 function draw_cloud(some_words) {
@@ -56,10 +56,9 @@ function draw_cloud(some_words) {
 		tab = tab.concat(some_words[i]);
 	}
 	$(document).ready(function() {
-	  $(".word_cloud_row_graph").jQCloud(tab);
-    });
-
-	 $('#word_cloud_cover').show();
+		$(".word_cloud_row_graph").jQCloud(tab);
+	});
+	$('#word_cloud_cover').show();
 }
 
 
