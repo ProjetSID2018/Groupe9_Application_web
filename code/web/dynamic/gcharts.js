23e39ab831f4e234a77ed6a64b345b0fcc0f13d5
diawahad <35219396+diawahad@users.noreply.github.com>
2018-01-15 16:55:02 +0100

    mise à jour des graphes de la page theme

--- code/web/dynamic/gcharts.js
+++ code/web/dynamic/gcharts.js
@@ -1,41 +1,80 @@
 google.charts.load('current', {'packages':['corechart', 'table']});
-google.charts.setOnLoadCallback(drawSeriesChart);
+/*google.charts.setOnLoadCallback(drawChart);
 
 var val_most_treated_themes = [
-		        ['Theme', 				'X', 		'Y',		"Ordre",		 'Effectif'],
-		        ['International',		 1,			 1,				0, 				81902307],
-		        ['Politique fr',		 2,			 2,				1, 				5523095],
-		        ['France',   			 3,			 3,				2, 				79716203],
-		        ['Economie',    		 4,			 4,				3,  			61801570],
-		        ['Science',    			 5,			 5,				4,  			73137148],
-		        ['Art et Culture',   	 6,			 6,				5,  			31090763],
-		        ['Sports',    			 7,			 7,				6, 				74856000],
-		        ['Santé',    		     8,			 8,				7, 				141850000],
+		        ['Theme', 					'Effectif'],
+            ['Politique fr',       5523095],
+            ['Art et Culture',    31090763],
+            ['Economie',          61801570],
+            ['Science',           73137148],
+            ['Sports',            74856000],
+            ['France',            79716203],
+		        ['International',		 	81902307],
+		        ['Santé',    		     141850000]
 		      ];
 
-function drawSeriesChart() {
+function drawChart() {
+  var data = google.visualization.arrayToDataTable(val_most_treated_themes);
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 1, { calc: "stringify",
+    sourceColumn: 1,
+    type: "string",
+    role: "annotation" 
+  }
+  ,2]);
 
-    var data = google.visualization.arrayToDataTable(val_most_treated_themes);
-    var options = {
-      title: 'Thèmes les plus traités',
-      hAxis: {title: 'X'},
-      vAxis: {title: 'Y'},
-      bubble: {textStyle: {fontSize: 11}}
+  var options = {
+    title: 'Thèmes les plus traités',
+    width: 600,
+    height: 400,
+    bar: {groupWidth: "95%"},
+    legend: { position: "none" },
+  };
+
+  var chart = new google.visualization.BarChart(document.getElementById('series_chart_theme'));
+  chart.draw(view, options);
+  };
+
+*/
+
+//google.charts.load("current", {packages:["corechart"]});
+var val_most_treated_themes = [
+            ['Theme',           'Effectif'],
+            ['Art et Culture',    31090763],
+            ['Economie',          61801570],
+            ['Science',           73137148],
+            ['Sports',            74856000],
+            ['France',            79716203],
+            ['International',     81902307],
+            ['Santé',            141850000]
+          ];
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(val_most_treated_themes);
+
+
+      var options = {   
+        hAxis : {
+          title : "Nombre d'articles",
+          minValue: 0
+        },
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
       };
-
-    var chart = new google.visualization.BubbleChart(document.getElementById('series_chart_theme'));
-    chart.draw(data, options);
-};
-
+      var chart = new google.visualization.BarChart(document.getElementById("series_chart_theme"));
+      chart.draw(data, options);
+  }
 
 google.charts.setOnLoadCallback(drawTable);
 // var cssClassNames = {'tableCell': 'table_trend_cell', 'headerCell': 'table_header_cell'};
 
 var words_associated_trend_1 = [
   ["Mots liés au thème",  "Tendance"],
-  ["Mot1", "Tendance1"],
-  ["Mot2", "Tendance2"],
-  ["Mot3", "Tendance3"]
+  ["Verbe", "Tendance1"],
+  ["Nom", "Tendance2"],
+  ["Adjectif", "Tendance3"]
 ];
 
 var words_associated_trend_2 = [
@@ -52,7 +91,6 @@ var words_associated_trend_3 = [
   ["Expression3", "Analyse3"]
 ];
 
-/*
 var json_table = {
    '1': {
        word_link: "Lorem",
@@ -63,7 +101,6 @@ var json_table = {
        weight: 10.5
      }
 }
-*/
 
 function drawTable() {
     // var options = { 'allowHtml': true, 'cssClassNames': cssClassNames };
