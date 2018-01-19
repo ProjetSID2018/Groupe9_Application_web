c8e2988eaf6068ef72fbb5e8688720e528fd3d3f
FlorenceGourmelon <florence.gourmelon@outlook.fr>
2018-01-19 16:54:49 +0000

    Update footer.js

--- code/web/dynamic/footer.js
+++ code/web/dynamic/footer.js
@@ -1,4 +1,5 @@
 
+// KB, PSB et FGD
 /*ajout du lien necessaire pour les logos (font-incone) du footer*/
 
 document.head.insertAdjacentHTML("beforeend",'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
