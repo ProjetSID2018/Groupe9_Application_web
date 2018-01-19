5aec7e0fc041e2071e13df532ea59e6ff4df77f1
Unknown <pou.1@hotmail.fr>
2018-01-19 09:00:55 +0100

    header vf

--- code/web/dynamic/header.js
+++ code/web/dynamic/header.js
@@ -3,9 +3,9 @@
 
 document.body.insertAdjacentHTML("afterBegin",
   '<header>'+
-    '<nav class="navbar navbar-default">'+
+    '<nav class="navbar navbar-default">'+ 
 
-    /*Navigation bar*/
+    /*Navigation bar*/    
       '<div id = "navBar" class="container">'+
         '<div class="navbar-header">'+
           '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#codebrainery-toggle-nav" aria-expanded="false">'+
@@ -32,18 +32,19 @@ document.body.insertAdjacentHTML("afterBegin",
             '<span class="glyphicon glyphicon-search"></span>'+
           '</button>' +
           '<div id="codebrainery-toggle-search" class="collapse navbar-collapse">' +
-            '<div class="col-sm-offset-3">'+
+            '<div class="col-lg-offset-3">'+
+            // '<div>'+
               '<div class="row">' +
-                '<div id="prefetch" class="col-sm-offset-4 col-sm-4">' +
+                '<div id="prefetch" class="col-xs-offset-4 col-xs-8 col-sm-offset-4 col-sm-8 col-md-offset-4 col-md-8 col-lg-offset-4 col-lg-8">' +
                   '<input id="searchBar_input_research" type="text" class="typeahead" placeholder="Recherche">' +
                 '</div>' +
               '</div>'+
               /*'<div class="row">' +
-                '<div class="col-sm-4">' +
+                '<div class="col-lg-4">' +
                   '<SELECT id="themeList_select_research" class="form-control" name="theme" size="1">' +
                     '<option> Tous les thèmes </option>' +
                     '<option> International </option>' +
-                    '<option> France </option>' +
+                    '<option> France </option>' +  
                     '<option> Economie </option>' +
                     '<option> Science/High-Tech </option>' +
                     '<option> Culture </option>' +
@@ -51,7 +52,7 @@ document.body.insertAdjacentHTML("afterBegin",
                     '<option> Sant&eacute; </option>' +
                   '</SELECT>' +
                 '</div>' +
-                '<div class="col-sm-4">' +
+                '<div class="col-lg-4">' +        
                   '<SELECT id="sourceList_input_research" class="form-control" name="Source" size="1">' +
                     '<option> Toutes les sources </option>' +
                     '<option> Le Figaro </option>' +
@@ -70,7 +71,7 @@ document.body.insertAdjacentHTML("afterBegin",
                     '<option> Le Nouvel Obs </option>' +
                   '</select>' +
                 '</div>' +
-                '<div class="col-sm-4">' +
+                '<div class="col-lg-4">' +        
                   '<SELECT id="freqList_input_research" class="form-control" name="frequence" size="1">' +
                     '<option>Traitement par semaine</option>' +
                     '<option>Traitement par mois</option>' +
@@ -80,7 +81,7 @@ document.body.insertAdjacentHTML("afterBegin",
               '</div>' +*/
 
               '<div class="row">' +
-                '<div class="col-sm-3">' +
+                '<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">' +
                   '<div class="form-group">' +
                     '<div class="input-group date" >' +
                       '<input type="text" class="form-control" id="startDate_input_research" style="cursor: pointer" onclick="new calendar(this);" value="Du :">' +
@@ -88,9 +89,9 @@ document.body.insertAdjacentHTML("afterBegin",
                         '<span class="glyphicon glyphicon-calendar"></span>' +
                       '</span>' +
                     '</div>' +
-                  '</div>' +
+                  '</div>' +    
                 '</div>' +
-                '<div class="col-sm-offset-1 col-sm-3">' +
+                '<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">' +
                   '<div class="form-group">' +
                     '<div class="input-group date" >' +
                       '<input type="text" class="form-control" id="endDate_input_research" style="cursor: pointer" onclick="new calendar(this);" value="Au :">' +
@@ -98,21 +99,21 @@ document.body.insertAdjacentHTML("afterBegin",
                         '<span class="glyphicon glyphicon-calendar"></span>' +
                       '</span>' +
                     '</div>' +
-                  '</div>' +
+                  '</div>' +    
                 '</div>' +
-                '<div class="col-sm-offset-1 col-sm-2">' +
-                  '<button id="buttonCancel_input_research" type="submit" class="btn btn-primary" onclick="window.location.reload(false)">Réinitialiser</button>'+
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">' +
+                  '<button id="buttonCancel_input_research" type="submit" class="btn btn-primary" onclick="window.location.relo2d(false)">Réinitialiser</button>'+
                 '</div>' +
-                '<div class="col-sm-2">' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">' +
                   '<button id="buttonResearch_input_research" type="submit" class="btn btn-primary">Valider</button>' +
                 '</div>'  +
               '</div>' +
             '</div>'+
-          '</div>'+
+          '</div>'+   
         '</div>'+
       '</div>' +
 
-    /*Theme choice*/
+    /*Theme choice*/ 
 
       '<div id = "themeBar" class = "container">' +
         '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#codebrainery-toggle-theme" aria-expanded="false">'+
@@ -125,26 +126,26 @@ document.body.insertAdjacentHTML("afterBegin",
             '</h5>' +
             '<div id="list_theme_div_theme" class="row">' +
               '<form>' +
-                '<div class="col-sm-2" + id="_international" value="_international">' +
-                  '<label><input id="international" class="radio" type="radio" name="optradio">International</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_international" value="_international">' +
+                  '<label><input id="_international" class="radio" type="radio" name="optradio">International</label>' +
                 '</div>' +
-                '<div class="col-sm-2" id="_france">' +
-                  '<label><input id="france" class="radio" type="radio" name="optradio">France</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_france">' +
+                  '<label><input id="_france" class="radio" type="radio" name="optradio">France</label>' +
                 '</div>' +
-                '<div class="col-sm-2" id="_economie">' +
-                  '<label><input id="economie" class="radio" type="radio" name="optradio">&Eacute;conomie</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_economie">' +
+                  '<label><input id="_economie" class="radio" type="radio" name="optradio">&Eacute;conomie</label>' +
                 '</div>' +
-                '<div class="col-sm-2" id="_sciences_high_tech">' +
-                  '<label><input id="sciences_high_tech" class="radio" type="radio" name="optradio">Science/High-tech</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_sciences_high_tech">' +
+                  '<label><input id="_sciences_high_tech" class="radio" type="radio" name="optradio">Science/High-tech</label>' +
                 '</div>' +
-                '<div class="col-sm-2" id="_arts_et_culture">' +
-                  '<label><input id="arts_et_culture" class="radio" type="radio" name="optradio">Art et Culture</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_arts_et_culture">' +
+                  '<label><input id="_arts_et_culture" class="radio" type="radio" name="optradio">Art et Culture</label>' +
                 '</div>' +
-                '<div class="col-sm-1" id="_sports">' +
-                  '<label><input id="sports" class="radio" type="radio" name="optradio">Sports</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_sports">' +
+                  '<label><input id="_sports" class="radio" type="radio" name="optradio">Sports</label>' +
                 '</div>'+
-                '<div class="col-sm-1" id="_sante">' +
-                   '<label><input id="sante" class="radio" type="radio" name="optradio">Sant&eacute;</label>' +
+                '<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" id="_sante">' +
+                   '<label><input id="_sante" class="radio" type="radio" name="optradio">Sant&eacute;</label>' +
                 '</div>' +
               '</form>' +
             '</div>' +
@@ -153,3 +154,4 @@ document.body.insertAdjacentHTML("afterBegin",
       '</div>' +
     '</nav>'+
   '</header>');
+
